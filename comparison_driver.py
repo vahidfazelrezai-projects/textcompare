3b936f7d1a03f99270f65f7fe10d2794ef80791f
Antonio Rivera <antonio.n.rivera@live.com>
2016-10-11 23:12:26 -0400

    comparison_driver.py now outputs the common words and frequencies for each pair, and also outputs the min and max value for each metric (as well as which pair of books achieved that value)

--- comparison_driver.py
+++ comparison_driver.py
@@ -60,21 +60,53 @@ def compare_files(directory_name):
   # Add the TF-ITTF (Term Frequency Inverse Total Term Frequency) metric
   metric.metrics['TF-ITTF'] = metric.Metric(metric.mult_fn, metric.unit_fn, metric.divide_by_magnitudes_fn, ittf_weight_fn)
 
+  low_scores = {}
+  low_pairs = {}
+  high_scores = {}
+  high_pairs = {}
+  for m in metric.metrics:
+    low_scores[m] = float("inf")
+    low_pairs[m] = "INITIAL VALUE"
+    high_scores[m] = float("-inf")
+    high_pairs[m] = "INITIAL VALUE"
+
   # Iterate over textdocs pairwise and compare them.
   for i in xrange(len(textdocs)):
     for j in xrange(i+1, len(textdocs)):
       doc1 = textdocs[i]
       doc2 = textdocs[j]
-      print "Difference between {0} and {1}".format(doc1.title, doc2.title)
+      print "Comparing {0} and {1}".format(doc1.title, doc2.title)
+      common_words = set(doc1.get_frequencies().keys()) & set(doc2.get_frequencies().keys())
+      f1 = doc1.get_frequencies()
+      f2 = doc2.get_frequencies()
+      print "Listing common words and their frequencies (the order of frequencies is the same as the order the book titles were listed)"
+      for word in common_words:
+        print "Word: '{0}'\tFrequencies: {1}, {2}".format(word, f1[word], f2[word])
       for m in metric.metrics:
         # perform logic for comparing doc1 and doc2
-        print "Using metric ", m, ":", metric.metrics[m].distance(doc1, doc2)
-      print
+        d = metric.metrics[m].distance(doc1, doc2)
+        print "Using metric ", m, ":", d
+        if d == -1:
+          # Ignore books with no words in common.
+          continue
+        if d < low_scores[m]:
+          low_scores[m] = d
+          low_pairs[m] = "{0} and {1}".format(doc1.title, doc2.title)
+        if d > high_scores[m]:
+          high_scores[m] = d
+          high_pairs[m] = "{0} and {1}".format(doc1.title, doc2.title)
+      print "--------------------------------------------------------------------------------\n"
+  print "Total Results Per Metric:"
+  for m in metric.metrics:
+    print m
+    print "Low:  {0} ({1})".format(low_scores[m], low_pairs[m])
+    print "High: {0} ({1})\n".format(high_scores[m], high_pairs[m])
 
 if __name__ == "__main__":
   default_filepath = "/Users/{0}/Dropbox (MIT)/children's books/books/".format(os.environ['USER'])
 
   if len(sys.argv) == 1:
+    print "NOTE: Not specifying an argument will only work for OS X. If using a differnt OS, please specify the filepath to the directory containing the books to be compared."
     print "No arguments were provided. Using default path of: {0}\n".format(default_filepath)
   elif len(sys.argv) > 2:
     print "Incorrect usage. Must specify 1 or 0 arguments. If 1 argument is specified, that is used as the filepath for finding the text files. Otherwise, {0} is used".format(default_filepath)
