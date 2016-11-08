b0ca324672958dbddf40ca884adfd695170438b0
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2016-11-08 11:07:40 -0500

    finish path 2

--- path.py
+++ path.py
@@ -17,10 +17,14 @@ def order_asymmetric2(graph):
     n = len(ids)
     nodes = graph['nodes']
     edges = graph['edges']
-    table = [[0] * n] * n
-    table[0] = [sum([edges[key] for key in edges.keys() if key[9] == ids[i]]) for i in range(n)]
-    scores = [(id, sum([edges[key] for key in edges.keys() if key[0] == id])) for id in ids]
-    # incomplete!
+    order = []
+    for i in range(n):
+        scores = [(id, sum([edges[key] for key in edges.keys() if (key[0] == id and key[1] in ids)])) for id in ids]
+        scores.sort(key=lambda x: x[1])
+        cur_id = scores[0][0]
+        order.append(cur_id)
+        ids = [id for id in ids if id != cur_id]
+    return [nodes[id][0] for id in order]
 
 if __name__ == '__main__':
     metric_name = 'Tversky index'
@@ -28,4 +32,4 @@ if __name__ == '__main__':
     textdocs, metric = compare.initialize(filepath)
     graph = compare.get_graph(textdocs, metric, metric_name)
     print order_asymmetric1(graph)
-    # print order_asymmetric2(graph)
+    print order_asymmetric2(graph)
